1730cbfb2c1489c028bae2cb5d68b6a3ce526ce7
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-07-27 12:21:50 +0530

    Not Completed

---
+++ amazon-client/src/Components/Header/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+      <header>
+          <nav>
+              <div className='left'>
+                  <div className="navLogo">
+                      
+                   </div>
+              </div>
+              <div className='right'>
+                  
+              </div>
+          </nav>
+   </header>
+  )
+}
+
+export default Navbar
