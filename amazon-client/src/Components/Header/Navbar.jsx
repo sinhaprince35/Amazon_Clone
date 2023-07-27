137cac155beea11b87eb5b3bdb057ca9800076a2
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-07-27 18:13:28 +0530

    not completed

--- amazon-client/src/Components/Header/Navbar.jsx
+++ amazon-client/src/Components/Header/Navbar.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
-import './navbar.css'
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import "./navbar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Avatar from '@mui/material/Avatar';
 
 const Navbar = () => {
   return (
-      <header>
-          <nav>
-              <div className='left'>
-                  <div className="navLogo">
-                      <img src='https://www.freepnglogos.com/uploads/amazon-png-logo-vector/amazon-symbol-png-logo-vector-9.png' alt='amazonLogo'/>
+    <header>
+      <nav>
+        <div className="left">
+          <div className="navLogo">
+            <img
+              src="https://www.freepnglogos.com/uploads/amazon-png-logo-vector/amazon-symbol-png-logo-vector-9.png"
+              alt="amazonLogo"
+            />
+          </div>
+          <div className="nav_searchbar">
+            <input type="text" name="" id="" placeholder="Amazon search" />
+            <div className="search_icon">
+              <SearchIcon id="search" />
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="nav_btn">
+            <a href="/">Sign in</a>
+          </div>
+          <div className="cart_btn">
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartIcon id="icon" />
+                      </Badge>
+                      <p>Cart</p>
                   </div>
-                  <div className="nav_searchbar">
-                      <input type='text' name='' id='' placeholder='Amazon search'/>
-                      <div className="search_icon">
-                          <SearchIcon />
-                      </div>
-                  </div>
-              </div>
-              <div className='right'>
-                  
-              </div>
-          </nav>
-   </header>
-  )
-}
+                  <Avatar className="avtar"/>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
