f759d2a3c3581b5b806c364cd0add8d8bc5074b3
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-07-27 19:47:34 +0530

    Not Completed

--- amazon-client/src/Components/Home/Banner.jsx
+++ amazon-client/src/Components/Home/Banner.jsx
@@ -4,10 +4,10 @@ import Carousel from 'react-material-ui-carousel'
 
 
 const data = [
-  "https://trak.in/wp-content/uploads/2020/08/IMG_20200808_191750.jpg",
-  "https://www.shopickr.com/wp-content/uploads/2015/09/amazon-india-discount-large-home-appliances-sale-2016.jpg",
-  "https://trak.in/wp-content/uploads/2019/04/Untitled-design-5-2.jpg",
-  "https://www.takemetechnically.com/wp-content/uploads/2023/05/amazon-great-summer-sale-2023-offers-up-to-70-off-on-gaming-laptops-and-gadgets-1024x576.webp",
+  "https://rukminim1.flixcart.com/flap/1680/280/image/1defb861e409319b.jpg?q=50",
+  "https://rukminim1.flixcart.com/flap/1680/280/image/685712c6cefb3c02.jpg?q=50",
+  "https://i0.wp.com/www.smartprix.com/bytes/wp-content/uploads/2022/09/GIF_amazon.png?ssl=1",
+  "https://infocoverage.com/wp-content/uploads/2021/10/Amazon-Great-Indian-Festival-Sale-Offers.jpg",
   "https://imageio.forbes.com/blogs-images/forbes-finds/files/2018/07/PD.jpg?format=jpg&width=1200"
 ]
 const Banner = () => {
