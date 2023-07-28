63c2a1a1e0136f7154d9d073f1cdc9d2cb3cd8bf
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-07-28 21:00:46 +0530

    80% Completed

--- amazon-server/constant/productsdata.js
+++ amazon-server/constant/productsdata.js
@@ -90,9 +90,49 @@ const products = [
       cost: 1124,
       discount: "40%",
     },
-    description: "",
+    description:
+      "Introducing Nova Professional Dryer with 1800 watts of powerful drying , so that you can get salon-like hair styling even at your home . This hair dryer features Advanced Ehd + Technology which ensures that only the right amount of heat is distributed on your hair and preventing any damage to your hair. The Shine and condition Techonlogy ensures that you hair is always silky and smooth. The Thermo protect temperature setting in this dryer also provides shine and conditions your hair, so that you have a Gorgeous looking hair style.",
     discount: "From ₹499",
     tagline: "Kubra, Nova & more",
+  },
+  {
+    id: "product6",
+    url: "https://img-prd-pim.poorvika.com/cdn-cgi/image/width=500,height=500,quality=75/product/oneplus-nord-3-5g-misty-green-128gb-8gb-ram-angled.png",
+    detailUrl:
+      "https://img-prd-pim.poorvika.com/cdn-cgi/image/width=500,height=500,quality=75/product/oneplus-nord-3-5g-misty-green-128gb-8gb-ram-angled.png",
+    title: {
+      shortTitle: "OnePlus Nord 3 5G",
+      longTitle: "OnePlus Nord 3 5G (Misty Green, 8GB RAM, 128GB Storage)",
+    },
+    price: {
+      mrp: 33999,
+      cost: 31999,
+      discount: "5%",
+    },
+    description:
+      "Get blazing Fast and Smooth performance with a bunch of RAM and our RAM-Vita algorithm. A big, beautiful display with gorgeous colors and a lightning-fast refresh rate.",
+    discount: "1699.95",
+    tagline: "A truly larger than life experience",
+  },
+  {
+    id: "product7",
+    url: "https://dukaan-core-file-service.s3.ap-southeast-1.amazonaws.com/upload_file_service/01a6f53a-292a-47ce-bc3e-d1fef00feeb5/vu-tv.png",
+    detailUrl:
+      "https://dukaan-core-file-service.s3.ap-southeast-1.amazonaws.com/upload_file_service/01a6f53a-292a-47ce-bc3e-d1fef00feeb5/vu-tv.png",
+    title: {
+      shortTitle: "VU GLOLED SMART TV",
+      longTitle:
+        "Vu 139 cm (55 inches) The Masterpiece Glo Series 4K Ultra HD Smart Android QLED TV 55QMP (Armani Gold) (2022 Model) | Built in 4.1 Speaker",
+    },
+    price: {
+      mrp: 60000,
+      cost: 58800,
+      discount: "2%",
+    },
+    description:
+      "Vu Televisions (also Vu Technologies) is a television brand and an LED TV and display manufacturer founded by Indian businessperson Devita Saraf, in United States in 2006. It is amongst top 10 largest-selling TV brand across e-commerce platforms in India.",
+    discount: "1200",
+    tagline: "A truly larger than life experience",
   },
 ];
 
