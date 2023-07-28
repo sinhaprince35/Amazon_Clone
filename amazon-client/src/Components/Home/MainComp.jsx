7017ab5c9dede931bd372961243db0f14e6e0752
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-07-28 06:20:08 +0530

    Not Completed

--- amazon-client/src/Components/Home/MainComp.jsx
+++ amazon-client/src/Components/Home/MainComp.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./maincomp.css";
 import Banner from "./Banner";
+import Slide from "./Slide";
 
 const MainComp = () => {
   return (
     <div className="home_section">
       <div className="banner_part">
         <Banner />
+        <Slide />
       </div>
     </div>
   );
