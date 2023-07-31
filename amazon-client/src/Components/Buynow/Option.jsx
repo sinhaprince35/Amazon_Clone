f0d32dc8a1f5863bb7b46030dec744fddc02c463
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-07-31 07:55:43 +0530

    Not Completed

--- amazon-client/src/Components/Buynow/Option.jsx
+++ amazon-client/src/Components/Buynow/Option.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 import { LoginContext } from "../Context/ContextProvider";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Option = ({ deletedata, get }) => {
-  
   const { account, setAccount } = useContext(LoginContext);
   const removedata = async (req, res) => {
     try {
@@ -10,23 +11,26 @@ const Option = ({ deletedata, get }) => {
         method: "DELETE",
         headers: {
           Accept: "application/json",
-          "Content-type": "application/json"
+          "Content-type": "application/json",
         },
-        credentials: "include"
+        credentials: "include",
       });
       const data = await res.json();
       console.log(data);
       if (res.status === 400 || !data) {
         console.log("Error");
       } else {
-        console.log("User delete")
+        console.log("User delete");
         setAccount(data);
+        toast.success("Item removed from your cart", {
+          position: "top-center",
+        });
         get();
       }
     } catch (error) {
       console.log("Error");
     }
-  }
+  };
   return (
     <div className="add_remove_select">
       <select>
@@ -35,11 +39,14 @@ const Option = ({ deletedata, get }) => {
         <option value="3">3</option>
         <option value="4">4</option>
       </select>
-      <p style={{ cursor: "pointer" }} onClick={()=>removedata(deletedata)}>Delete</p>
+      <p style={{ cursor: "pointer" }} onClick={() => removedata(deletedata)}>
+        Delete
+      </p>
       <span>|</span>
       <p className="forremovemedia">Save or Later</p>
       <span>|</span>
       <p className="forremovemedia">See More like this</p>
+      <ToastContainer />
     </div>
   );
 };
